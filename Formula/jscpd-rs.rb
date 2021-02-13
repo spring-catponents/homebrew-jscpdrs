93d3f7ea5b07507aebacafc4f7c3816f45f47b1b
jiangWeixian <jiangweixian1994@gmail.com>
2021-02-13 22:34:36 +0800

    🐛 dead link

--- Formula/jscpd-rs.rb
+++ Formula/jscpd-rs.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class jscpdrs < Formula
   desc "jscpd detect write in rust"
-  homepage "https://github.com/spring-catponents/jscpd-rs"
+  homepage "https://github.com/spring-catponents/jscpdrs"
   url "https://github.com/spring-catponents/jscpd-rs/releases/latest/download/jscpd-rs.tar.gz"
   sha256 "7ffd14b257005fcb7c704f538392ce2daee02d0afbf127007a5420fa1719a941"
   version "0.1.0"
