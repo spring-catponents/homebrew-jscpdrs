1e65ad9a2c62274d00575207b2b1e7e02221cc6a
jiangWeixian <jiangweixian1994@gmail.com>
2021-02-14 10:15:38 +0800

    🔧 update tar file path

--- Formula/jscpdrs.rb
+++ Formula/jscpdrs.rb
@@ -4,8 +4,8 @@
 class Jscpdrs < Formula
   desc "jscpd detect write in rust"
   homepage "https://github.com/spring-catponents/jscpdrs"
-  url "https://github.com/spring-catponents/jscpd-rs/releases/latest/download/jscpd-rs.tar.gz"
-  sha256 "7ffd14b257005fcb7c704f538392ce2daee02d0afbf127007a5420fa1719a941"
+  url "https://github.com/spring-catponents/jscpd-rs/releases/latest/download/jscpdrs.tar.gz"
+  sha256 "92cfdd03550d0c462874dd4d2dc0f231036112b38c910a3c1e246f804ca36f28"
   version "0.1.0"
 
   def install
